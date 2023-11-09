335b3a54a89de41d810d3ee95488a0a9c9c6dfa5
Fideltodayy <fidelotieno11@gmail.com>
2023-11-08 16:16:34 -0800

    TAsk number 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+import hidden_4
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module"""
+    names = dir(hidden_4)
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
