d16bc19b7b5ea9a9a5ce39ab2923f76bf7fc290e
Fideltodayy <fidelotieno11@gmail.com>
2023-11-21 22:15:21 +0300

    TAsk 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,18 @@
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for _ in range(self.__size):
+            print("#" * self.__size)
