c72400d0d65399e8f525d4599485257920fea544
Fideltodayy <fidelotieno11@gmail.com>
2023-11-21 22:41:34 +0300

    COmments

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,42 +1,58 @@
 #!/usr/bin/python3
-"""This module defines a Square class with a size attribute, an area method, a print method, size getter and setter, and a class method."""
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square with a size attribute, an area method, a print method, size getter and setter, and a class method."""
+    """Represent a square."""
 
-    def __init__(self, size):
-        """
-        Initializes the square with a size.
-
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
         Args:
-            size (int): The size of the square.
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
-        self.__size = size
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Gets the size of the square."""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
-        """Returns the area of the square."""
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Prints the square with the # character."""
-        for _ in range(self.__size):
-            print("#" * self.__size)
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
 
-    @classmethod
-    def bigger_or_equal(cls, rect_1, rect_2):
-        """Returns the bigger square or the first one if they are equal."""
-        if rect_1.area() > rect_2.area():
-            return rect_1
-        else:
-            return rect_2
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
