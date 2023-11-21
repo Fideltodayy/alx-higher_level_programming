05e54dd7b9c3e3dde92df2c7093ab93f90b5780d
Fideltodayy <fidelotieno11@gmail.com>
2023-11-21 22:39:53 +0300

    COmments

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a Square class with a size attribute, an area method, a print method, and size getter and setter."""
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square with a size attribute, an area method, a print method, and size getter and setter."""
+    """Represent a square."""
 
-    def __init__(self, size):
-        """
-        Initializes the square with a size.
-
+    def __init__(self, size=0):
+        """Initialize a new square.
         Args:
-            size (int): The size of the square.
+            size (int): The size of the new square.
         """
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
-        """Gets the size of the square."""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Returns the area of the square."""
-        return self.__size ** 2
-
-    def my_print(self):
-        """Prints the square with the # character."""
-        for _ in range(self.__size):
-            print("#" * self.__size)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
