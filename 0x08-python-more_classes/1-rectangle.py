b898f8d33b06f0ff658d923c8f26261d4527468a
Fideltodayy <fidelotieno11@gmail.com>
2023-11-28 04:42:51 +0300

    Added 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,10 @@
+# 1-rectangle.py
+#!/usr/bin/python3
+"""Defines a Rectangle class with width and height attributes."""
+
+class Rectangle:
+    """Represent a rectangle with width and height."""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
