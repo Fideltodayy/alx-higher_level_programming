c320341459c1c4b7f6a063dc10ff73880856c112
Fideltodayy <fidelotieno11@gmail.com>
2023-11-28 04:46:49 +0300

    Added # 2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class with validation checks."""
+
+class Rectangle:
+    """Represent a rectangle with validation for width and height."""
+    def __init__(self, width, height):
+        if isinstance(width, int) and width > 0:
+            self.width = width
+        else:
+            raise ValueError("Width must be a positive integer")
+        if isinstance(height, int) and height > 0:
+            self.height = height
+        else:
+            raise ValueError("Height must be a positive integer")
